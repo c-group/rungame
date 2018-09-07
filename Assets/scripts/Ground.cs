11c3d39b8f5dcc9aaab1283c9339709c51f22a44
稲垣俊栄 <35315537+inagaki-toshihiro@users.noreply.github.com>
2018-09-07 11:50:02 +0900

    inagaki

---
+++ Assets/scripts/Ground.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+
+    // 変数の定義と初期化
+    public float scroll = -0.5f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position += new Vector3(scroll, 0, 0);
+    }
+}
