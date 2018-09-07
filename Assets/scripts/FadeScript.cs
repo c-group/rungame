e3d30319b79c57139053c0c4eab47764574d9079
zakoji <35319644+zakoji@users.noreply.github.com>
2018-09-07 11:34:21 +0900

    zakoji

--- Assets/scripts/FadeScript.cs
+++ Assets/scripts/FadeScript.cs
@@ -13,11 +13,11 @@ public class FadeScript : MonoBehaviour
     public void MoveUp()
     {
        
-     iTween.MoveTo(gameObject, iTween.Hash("y", 500, "time", 1));
+     iTween.MoveTo(gameObject, iTween.Hash("y", 1500, "time", 1));
     }
     public void MoveDown()
     {
 
-        iTween.MoveTo(gameObject, iTween.Hash("y", 140, "time", 2));
+        iTween.MoveTo(gameObject, iTween.Hash("y", 600, "time", 2));
     }
 }
