64a1734c635b3477d9c3a937b84d65131b2b805d
c-group <41134182+c-group@users.noreply.github.com>
2018-09-08 18:54:35 +0900

    zakoji
    gameover追加

---
+++ Assets/scripts/Destroy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroy : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //衝突判定
+      
+        {
+           
+            Destroy(this.gameObject);
+        }
+    }
+}
