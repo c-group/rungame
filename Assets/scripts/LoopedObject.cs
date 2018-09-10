2666592959fb37efeb08b8f50ea8e2fd44b303f6
zakoji <35319644+zakoji@users.noreply.github.com>
2018-09-10 17:04:43 +0900

    zokoji
    Stage2 groundループ追加

---
+++ Assets/scripts/LoopedObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class LoopedObject : MonoBehaviour
+{
+    private void LateUpdate()
+    {
+        // ループ管理システムがあれば、位置の計算をしてもらおう。
+        // 通常の操作処理をUpdate()で行っているので、とりあえずLateUpdate()で処理しているよ。
+        if (LoopedField.Instance != null)
+        {
+            transform.position = LoopedField.Instance.ComputePosition(transform.position);
+        }
+    }
+
+} // class LoopedObject
---
+++ Assets/scripts/createobj.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class createobj: MonoBehaviour {
+
+    public GameObject obj; // 生成するオブジェクト
+
+    public int createNumbers; // 生成する数
+    public int space; // 生成する間隔
+    public Vector3 createPos; // 生成する場所
+
+    // Use this for initialization
+    void Start()
+    {
+        CreateObject(); // 生成メソッド実行
+    }
+
+    // 生成メソッド
+    void CreateObject()
+    {
+        for (int i = 0; i < createNumbers; i++)
+        {
+            Instantiate(obj, createPos + new Vector3(i + space * i, 0, 0), Quaternion.identity);
+        }
+    }
+}
