628da0d4aea2968a3e3b2c0733fae90037680f60
稲垣俊栄 <35315537+inagaki-toshihiro@users.noreply.github.com>
2018-09-16 02:25:37 +0900

    inagaki
    キャラ選択時のPriest，Wizardボタンの有効化，及び選択時ボイスを実装
    上記に伴ってステージ移行後Priest,Wizardもキャラ本体のみ実装(キーボードでのジャンプと素振りのみ)
    
    フラグ管理実装
    概要:マップよりキャラクター選択時に，そのキャラクターのフラグを立てる．ステージ移行後，フラグの立っているキャラクターを生成する．
    リザルト画面で，リトライを選択時はフラグをそのまま，メニューに戻るでフラグをリセットし，他のキャラの選択を可能にする．

---
+++ Assets/scripts/Character_Creation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Creation : MonoBehaviour {
+
+    public GameObject Soldier;
+    public GameObject Priest;
+    public GameObject Wizard;
+
+    // Use this for initialization
+    void Start()
+    {
+        int SoldierFlag = Character_Flag.GetS_Flag();
+        int PriestFlag = Character_Flag.GetP_Flag();
+        int WizardFlag = Character_Flag.GetW_Flag();
+
+        if(SoldierFlag == 1)
+        {
+        Instantiate(Soldier);        
+        }
+        else if (PriestFlag == 1)
+        {
+            Instantiate(Priest);
+        }
+        else if (WizardFlag == 1)
+        {
+            Instantiate(Wizard);
+        }
+    }
+
+
+}
---
+++ Assets/scripts/Character_Flag.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Flag : MonoBehaviour {
+
+
+    public static int Soldier = 0;
+    public static int Priest = 0;
+    public static int Wizard = 0;
+
+    public void SoldierFlag()
+    {
+        Soldier++;
+    }
+
+    public void PriestFlag()
+    {
+        Priest++;
+    }
+
+    public void WizardFlag()
+    {
+        Wizard++;
+    }
+
+    public static int GetS_Flag()
+    {
+        return Soldier;
+    }
+
+    public static int GetP_Flag()
+    {
+        return Priest;
+    }
+
+    public static int GetW_Flag()
+    {
+        return Wizard;
+    }
+
+    public void SetReset()
+    {
+        Soldier = 0;
+        Priest = 0;
+        Wizard = 0;
+    }
+
+
+}
