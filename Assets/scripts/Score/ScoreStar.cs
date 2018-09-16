e4ffdfb80a9db63b4d13312b61ec03dafcc5fa67
稲垣俊栄 <35315537+inagaki-toshihiro@users.noreply.github.com>
2018-09-16 15:56:43 +0900

    inagaki
    スコア星点灯実装

---
+++ Assets/scripts/Score/ScoreStar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ScoreStar : MonoBehaviour {
+
+    public int Lighiting_up_point = 0;
+
+	void Start () {
+        int result = Score.getScore();
+        
+        if(Lighiting_up_point <= result)
+        {
+            this.GetComponent<Image>().color = new Color(1f, 1f, 0f, 1f);
+        }
+	}
+	
+
+}
