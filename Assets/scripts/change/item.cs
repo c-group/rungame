fae20f10a5ddddfb3f6f3258d1fd8af6f6f10ef3
c-group <41134182+c-group@users.noreply.github.com>
2018-09-27 20:23:59 +0900

    zakoji
    bgm追加

---
+++ Assets/scripts/change/item.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Button))]
+public class item : UIBehaviour
+{
+    protected override void Start()
+    {
+        base.Start();
+
+        // Buttonクリック時、OnClickメソッドを呼び出す
+        GetComponent<Button>().onClick.AddListener(OnClick);
+
+
+    }
+
+    void OnClick()
+    {
+        FadeManager.Instance.LoadScene("item", 1.0f);
+
+    }
+}
---
+++ Assets/scripts/change/score.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Button))]
+public class score : UIBehaviour
+{
+    protected override void Start()
+    {
+        base.Start();
+
+        // Buttonクリック時、OnClickメソッドを呼び出す
+        GetComponent<Button>().onClick.AddListener(OnClick);
+
+
+    }
+
+    void OnClick()
+    {
+        FadeManager.Instance.LoadScene("scoreranking", 1.0f);
+
+    }
+}
