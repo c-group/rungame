9b17be61f96f15186561bdc03f1a3a5c848af7a4
c-group <41134182+c-group@users.noreply.github.com>
2018-09-27 17:38:54 +0900

    zakoji
    アイテム画面作成
    スコアランキング画面のスクロール実装

---
+++ Assets/scripts/FadeScript2.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeScript2 : MonoBehaviour
+{
+
+    public GameObject obj;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+    public void MoveUp()
+    {
+
+        iTween.MoveTo(gameObject, iTween.Hash("y", 480, "time", 1.5));
+    }
+    public void MoveDown()
+    {
+
+        iTween.MoveTo(gameObject, iTween.Hash("y", -500, "time", 1));
+    }
+}
