9c83404fba1c338ba173db9d9725af08ac27a83b
c-group <41134182+c-group@users.noreply.github.com>
2018-10-24 15:06:39 +0900

    zakoji
    樽追加

--- Assets/scripts/Rotate.cs
+++ Assets/scripts/Rotate.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class Rotate : MonoBehaviour
 {
+    public int x;
+    public int y;
+    public int z;
 
     void Update()
     {
-        transform.Rotate(new Vector3(0, 0, 5));
+        transform.Rotate(new Vector3(x, y, z));
     }
 }
---
+++ Assets/scripts/create_barrel.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// InvokeRepeating関数を使った、一定時間ごとにオブジェクトを生成するクラス
+/// </summary>
+public class create_barrel : MonoBehaviour
+{
+    /// <summary>
+    /// privateな変数をインスペクターから設定できるようにする
+    /// </summary>
+    [SerializeField]
+    /// <summary>
+    /// プレハブ
+    /// </summary>
+    private GameObject prefab;
+    /// <summary>
+    /// 待ち時間
+    /// </summary>
+    public float waitingTime;
+    /// <summary>
+    /// 初期化
+    /// </summary>
+    /// 
+    public int x;
+    public int y;
+    
+
+    void Awake()
+    {
+        // InvokeRepeating("関数名",初回呼出までの遅延秒数,次回呼出までの遅延秒数)
+        InvokeRepeating("Create", waitingTime, waitingTime);
+    }
+    /// <summary>
+    /// オブジェクトの生成
+    /// </summary>
+    void Create()
+    {
+        // インスタンス生成
+        Instantiate(prefab, new Vector3(x, y, 2), Quaternion.Euler(-71, -76, 72));
+    }
+}
