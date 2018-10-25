ca7cd00e275ce8846c881818154b5be3812f0afa
c-group <41134182+c-group@users.noreply.github.com>
2018-10-25 11:22:40 +0900

    zakoji
    島　大砲追加

---
+++ Assets/scripts/Shooting.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+
+    // bullet prefab
+    public GameObject bullet;
+
+    // 弾丸発射点
+    public Transform muzzle;
+
+    // 弾丸の速度
+    public float speed = 1000;
+
+    public float waitingTime=20;
+
+    private AudioSource sound01;
+
+    // Use this for initialization
+    void Awake()
+    {
+        // InvokeRepeating("関数名",初回呼出までの遅延秒数,次回呼出までの遅延秒数)
+        InvokeRepeating("Create", waitingTime, waitingTime);
+    }
+
+    void Start()
+    {
+        //AudioSourceコンポーネントを取得し、変数に格納
+        sound01 = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Create()
+    {
+        
+      
+
+            // 弾丸の複製
+            GameObject bullets = Instantiate(bullet) as GameObject;
+
+            Vector3 force;
+
+            force = this.gameObject.transform.forward * speed;
+
+            // Rigidbodyに力を加えて発射
+            bullets.GetComponent<Rigidbody>().AddForce(force);
+
+            // 弾丸の位置を調整
+            bullets.transform.position = muzzle.position;
+
+            sound01.PlayOneShot(sound01.clip);
+
+
+    }
+}
